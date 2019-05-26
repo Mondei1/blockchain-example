f9d9e72dcb85810f1119e0c4844011e7f071d341
Mondei1 <klier.nicolas@protonmail.com>
2019-05-26 16:44:49 +0200

    Some improvements to the code Compiled js file is now in the root directory

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ Das ist nur ein kleines Beispiel einer Blockchain, geschrieben für eine Schul-F
 [Hier](https://github.com/mondei1/blockchain-example/archive/master.zip) klicken um den Source Code herunterzuladen.
 
 ## Abhängigkeiten
-* [Node](https://nodejs.org/en/) (>v8) zum ausführen des Programmes
+* [Node](https://nodejs.org/en/) zum ausführen des Programmes
 
 Im Terminal folgende Befehle ausführen: 
 1. `npm install`
+2. `npm install -g typescript`
 2. `tsc`
-3. `cd dist/`
 4. `node app.js` oder auch `node app.js --help` um eine Liste an Argumenten zu erhalten
 
 Danach sollte das Programm ohne Probleme starten und ein paar Blöcke erstellen.
@@ -19,7 +19,3 @@ Danach sollte das Programm ohne Probleme starten und ein paar Blöcke erstellen.
 ## Lizenz
 Dieses Projekt ist mit der `MIT-License` lizenziert. Bedeutet du kannst alles mit machen. Falls du noch mehr
 erfahren möchtest klicke [hier](https://choosealicense.com/licenses/mit/).
-
-### PS
-Ich weiß das der Code noch einige Fehler hat (z. B. wenn ein Block zwei Fehler hat wird er am Ende doppelt gelistet).
-Ich habe mich aber entschieden diese Fehler nicht zu beheben um den Code nicht noch komplexer zu machen. Das was er machen soll, macht er ja ^^.
--- app.ts
+++ app.ts
@@ -69,27 +69,11 @@ class Block {
                 // Schleife die nur endet, wenn der Hash mit x Nullen anfängt. (difficulty)
                 while(!this.hash.startsWith(this.blockchain.getHashStart())) {
                     this.nonce++;
-                    this.hash = await this.sha256(this.mergeData());
+                    this.hash = await sha256(this.mergeData());
                 }
                 const endTime = new Date().getTime();
                 this.calcTime = endTime - startTime;
                 resolve(this.hash);
-            } catch(err) {
-                reject(err)
-            }            
-        })
-    }
-
-    /**
-     * Diese Funktion errechnet den Hash eines Blockes (SHA-256)
-     */
-    async sha256(toHash: string): Promise<string> {
-        return new Promise<string>((resolve, reject) => {
-            try {
-                let hash = crypto.createHash('sha256');
-                hash.update(toHash, 'utf8');
-                const calcHash = hash.digest('hex');
-                resolve(calcHash);
             } catch(err) {
                 reject(err)
             }            
@@ -142,6 +126,7 @@ class Chain {
             const genesisBlock: Block = new Block(this, 0, "", "admin", "admin2", "Genesis block");
             await genesisBlock.calculateHash();
             this.chain.push(genesisBlock);
+            console.log("Genesis Block erstellt #0: " + genesisBlock.hash);
         }
     }
 
@@ -168,7 +153,7 @@ class Chain {
                 if(block.hash.startsWith(this.getHashStart())) {
                     // Stimmt der Hash allgemein?
                     const blockHash: string = block.hash;
-                    const newHash = await block.sha256(block.mergeData());
+                    const newHash = await sha256(block.mergeData());
                     if(newHash == blockHash) {
                         console.log(chalk.green("\t-> Hash ist korrekt"))
                     } else {
@@ -194,32 +179,32 @@ class Chain {
 }
 
 /**
- * Erstellt eine zufällige Zahl zwischen `min` und `max`
- */
-function random(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-/**
  * Asyn­chrone Funktion die direkt beim Start gestartet wird.
  */
 async function run() {
+    const startTime = Date.now();
     // Kommandozeile Parameter
     const user_arguments: any = minimist(process.argv.slice(2));
     const difficulty = user_arguments.d || 4;
-    const with_errors: boolean = user_arguments.e || false;
     const random_blocks: number = user_arguments.z || 0;
     const no_validation: boolean = user_arguments.novalid || false;
     const show_help: boolean = user_arguments.help || false;
+    const hide_blockchain: boolean = user_arguments.noshow || false;
+    let manipulate_blocks = user_arguments.m || [-1];
+    if(typeof manipulate_blocks === 'number') manipulate_blocks = [manipulate_blocks];  // In einen Array umwandeln
 
     if(show_help) {
         console.log("\nEine Liste an Argumenten die benutzt werden können um die Erstellung der Blockchain zu verändern:\n--------------------");
         console.log("Nutzen: node app.js [ARGUMENTE]");
         console.log(chalk.bold("\nARGUMENTE"));
         console.log("\t-d [DIFFICULTY]\t - Legt fest wie viele Nullen der Hash am Anfang haben soll (standard: 4)");
-        console.log("\n\t-e\t\t - Die Blockchain wird nach der Erstellung verändert (standard: false)");
         console.log("\n\t-z [ZAHL]\t - Anzahl zufällig erstellter Blöcker (standard: 0)");
-        console.log("\n\t--novalid\t - Keine Validierung der Blockchain durchführen (standard: false)\n")
+        console.log("\n\t-m [BLOCK INDEX] - Manipuliert einen bestimmten Block (standard: keiner)\n");
+        console.log("\n\t--novalid\t - Keine Validierung der Blockchain durchführen (standard: false)");
+        console.log("\n\t--noshow\t - Zeigt die Blockchain nicht an (standard: false)");
+        console.log(chalk.bold("\nBEISPIELE"));
+        console.log("\tnode app.js -d 2 -z 900 -m 50 -m 51\t - Schwierigkeit ist 2, erstellt 900 zufällige Blöcke und manipuliert Block 50 und 51.");
+        console.log("\tnode app.js -d 3\t\t\t - Erstellt alle sechs Standard Blöcke mit einer Schwierigkeit von 3.");
         return;
     }
 
@@ -231,7 +216,7 @@ async function run() {
     }
     const blockchain: Chain = new Chain(difficulty);
     
-    console.log(`Erstelle Blöcke (difficulty: ${difficulty} | mit fehlern? ${with_errors}) ...\n============================`)
+    console.log(`Erstelle Blöcke ...\n============================`)
     await blockchain.generateGenesisBlock();  // Erstelle Genesis Block (setup)
 
     // Konversation 1
@@ -245,9 +230,9 @@ async function run() {
     await blockchain.addBlock(USERS.USER3, USERS.USER1, "Sag mal, kannst du mir mal die Hausaufgaben von gestern schicken. Bin zu faul die jetzt zu machen ¯\\_(ツ)_/¯");
     await blockchain.addBlock(USERS.USER1, USERS.USER3, "Hab die selber nicht. Leider ist unser alleswisser " + USERS.USER2 + " noch im Urlaub, habe ihn aber gerade gefragt aber ich hab noch keine Antwort.")
 
-    // Zufällige Nachrichten
+    // Zufällige Nachrichten, wenn -z gegeben ist.
     for(let i = 0; i < random_blocks; i++) {
-        const people: Array<string> = ["Mondei1", "GoodbyeDarknes", "youngLarry", "thatminer", "Ray", "Eisenfell", "locumat", "Mori", "DerTim"];
+        const people: Array<string> = ["Mondei1", "GoodbyeDarkness", "youngLarry", "thatminer", "Ray", "Eisenfell", "locumat", "Mori", "DerTim", "fck_cDu", "stanley", "theNarrator"];
         const messages: Array<string> = [
             "Das ist eine zufällige Nachricht die ich an dich sende.",
             "Was für eine Zufall nicht wahr?",
@@ -259,6 +244,7 @@ async function run() {
             "Doch, also ich glaube an Zufall.",
             "Wusstest du das ein Computer keinen echten Zufall erstellen kann?",
             "Zufallsrechnung in Mathe ist langweilig.",
+            "Zufällig habe ich eine Nachricht geschrieben und zufällig an dich gesendet.",
             "Vielleicht gewinne per Zufall im Lotto."
         ]
         const from: string = people[random(0, people.length-1).toFixed(0)];
@@ -267,24 +253,84 @@ async function run() {
         await blockchain.addBlock(from, to, message);
     }
 
-    if(with_errors) {
-        blockchain.chain[1].hash = "0000ab1bcff2";
-        blockchain.chain[2].createdAt = 1557165712342;
-        blockchain.chain[5].message = "Kannst du mir mal die Hausaufgaben schicken du Hur**sohn?";
+    // Manipuliert bestimmte Blöcke
+    if(manipulate_blocks.length > 0) {
+        for(let i = 0; i < manipulate_blocks.length; i++) {
+            if(manipulate_blocks[i] != -1) {
+                try {
+                    const currentBlock = blockchain.chain[manipulate_blocks[i]];
+                    const action = random(0, 2).toFixed(0);
+                    switch(action) {
+                        // Ändere Hash des Blockes
+                        case '0':
+                            currentBlock.hash = await sha256(random(1, 99999).toString()); // Erstellt einen zufälligen Hash
+                            break;
+                        // Ändere Nachricht
+                        case '1':
+                            currentBlock.message = "Die Nachricht wurde manipuliert.";
+                            break;
+                        // Ändere previous_hash
+                        case '2':
+                            currentBlock.previous_hash = await sha256(random(1, 99999).toString()); // Erstellt einen zufälligen Hash
+                            break;
+                    }
+                    console.log("Block " + currentBlock.index + " manipuliert.")
+                } catch(err) {
+                    console.log(chalk.red(`Block #${manipulate_blocks[i]} wurde nicht gefunden!`));
+                }    
+            }
+        }
     }
 
-    console.log(blockchain.chain);
+    // Zeigt die Blockchain an, wenn --noshow nicht gegeben ist.
+    if(!hide_blockchain) {
+        console.log(blockchain.chain);
+    }
 
+    // Führt überprüfung durch, wenn --novalid nicht gegeben ist.
     if(!no_validation) {
         console.log("\n\n\n");
-        const blockchainValidation: Array<Block> = await blockchain.validate();
+
+        // Validiert die Blockchain und entfernt Duplikate
+        const blockchainValidation: Array<Block> = (await blockchain.validate()).filter(function(elem, index, self) {
+            return index === self.indexOf(elem);
+        });
+
         if(blockchainValidation.length == 0) {
             console.log(chalk.bold(chalk.green("Blockchain ist komplett valide!")));
         } else {
             console.log(chalk.bold(chalk.red(`Blockchain hat ${blockchainValidation.length} fehlerhafte Blöcke:`)));
-            console.log(blockchainValidation);
+            blockchainValidation.forEach((block) => {
+                console.log(`  - Block #${block.index}`)
+            })
         }
     }
+    const finishTime = Date.now();
+    console.log(`Laufzeit: ${((finishTime-startTime)/1000).toFixed(2)}s`)
+}
+
+// ------------------------- [ANDERE FUNKTIONEN] -------------------------
+/**
+ * Erstellt eine zufällige Zahl zwischen `min` und `max`
+ */
+function random(min, max): number {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Diese Funktion errechnet den Hash eines Blockes (SHA-256)
+ */
+async function sha256(toHash: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+        try {
+            let hash = crypto.createHash('sha256');
+            hash.update(toHash, 'utf8');
+            const calcHash = hash.digest('hex');
+            resolve(calcHash);
+        } catch(err) {
+            reject(err)
+        }            
+    })
 }
 
 run();
